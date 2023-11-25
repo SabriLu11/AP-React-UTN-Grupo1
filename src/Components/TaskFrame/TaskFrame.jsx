f5dba7009573a6d73c41afcb057659ad17978781
Guillermina-Salinas <guillerminasalinas.ok@gmail.com>
2023-11-25 17:14:34 -0300

    Se agrega mensaje por si no hay tareas para mostrar

--- src/Components/TaskFrame/TaskFrame.jsx
+++ src/Components/TaskFrame/TaskFrame.jsx
@@ -1,8 +1,23 @@
 import './TaskFrame.css';
 import TaskList from '../TaskList/TaskList'
 import { useThemeContext } from '../../context/ThemeContext';
+import { useState } from "react";
+import { useEffect } from "react";
 
 const TaskFrame = ({ list, handleDelete, setValue }) => {
+    
+    //Si la lista de tareas está vacía
+    const [noTask, setNoTask] = useState('');
+
+    useEffect(() => {
+        if (list.length === 0) {
+          setNoTask('Aún no hay tareas para hoy!');
+        } else {
+          setNoTask('');
+        }
+      }, [list]);
+
+    
     //manejo del tema
     const {contextTheme} = useThemeContext()
     const themeClass = contextTheme === 'Dark' ? 'Dark' : 'Light';
@@ -10,17 +25,24 @@ const TaskFrame = ({ list, handleDelete, setValue }) => {
         <>
             <table className='frame' id={`list${themeClass}`}>
                 {
-                    list.map((tarea, index) => (
-                        <tr>
-                            <TaskList
-                                key={index}
-                                tarea={tarea}
-                                handleDelete={() => handleDelete(tarea)}
-                                setValue={setValue}
-                            />
-                            <div className="taskSeparator" />
-                        </tr>
-                    ))
+                    noTask ? (
+                        <div className="notask">
+                            <p>{noTask}</p>
+                        </div>
+                        
+                      ):(
+                        list.map((tarea, index) => (
+                            <tr>
+                                <TaskList
+                                    key={index}
+                                    tarea={tarea}
+                                    handleDelete={() => handleDelete(tarea)}
+                                    setValue={setValue}
+                                />
+                                <div className="taskSeparator" />
+                            </tr>
+                            ))
+                        )
                 }
 
                 <tr className='buttonsFrame' id={`list${themeClass}`}>
